fc3e0b707e309f669ad0a5c0eed9c6993e70d38b
Raph <raphaelmercie4@gmail.com>
2022-06-12 00:25:51 +0200

    add all user interactions

---
+++ app/src/main/java/com/raphaelMrci/circlebar/PreparingActivity.kt
@@ -0,0 +1,11 @@
+package com.raphaelMrci.circlebar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreparingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preparing)
+    }
+}
---
+++ app/src/main/java/com/raphaelMrci/circlebar/ValidateActivity.kt
@@ -0,0 +1,11 @@
+package com.raphaelMrci.circlebar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ValidateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_validate)
+    }
+}
