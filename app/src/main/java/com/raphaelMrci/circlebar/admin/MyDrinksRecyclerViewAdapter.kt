2b4f9cc9811a80cbc055ba9bd07b60a1d092142a
Raph <raphaelmercie4@gmail.com>
2022-06-12 21:58:43 +0200

    add drink delte button

--- app/src/main/java/com/raphaelMrci/circlebar/admin/MyDrinksRecyclerViewAdapter.kt
+++ app/src/main/java/com/raphaelMrci/circlebar/admin/MyDrinksRecyclerViewAdapter.kt
@@ -1,21 +1,31 @@
 package com.raphaelMrci.circlebar.admin
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
+import com.raphaelMrci.circlebar.LOGIN_TOKEN
 
 import com.raphaelMrci.circlebar.databinding.FragmentDrinksBinding
 import com.raphaelMrci.circlebar.models.Drink
+import com.raphaelMrci.circlebar.network.ApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class MyDrinksRecyclerViewAdapter(
     private val values: MutableList<Drink>,
     var myContext: Context
-) : RecyclerView.Adapter<MyDrinksRecyclerViewAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<MyDrinksRecyclerViewAdapter.ViewHolder>(), CoroutineScope {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
@@ -40,6 +50,44 @@ class MyDrinksRecyclerViewAdapter(
                 putExtra("id", item.id)
             })
         }
+        holder.deleteBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(myContext)
+            builder.setTitle("Delete ${item.name} ?")
+            builder.setMessage("Are you sure you want to delete tis drink ? Be sure it is not included on an existing cocktail before deleting it.")
+            builder.setPositiveButton("Supprimer") { _,_ ->
+                item.id?.let { it1 -> deleteDrink(it1) }
+            }
+            builder.create().show()
+            // builder.setNegativeButton("Annuler", )
+        }
+    }
+
+    private fun deleteDrink(id: Int) {
+        launch(Dispatchers.Main) {
+            try {
+                val result = ApiClient.apiService.deleteDrink(id, "Bearer $LOGIN_TOKEN")
+
+                if (result.isSuccessful) {
+                    Toast.makeText(
+                        myContext,
+                        "Drink successfully deleted.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        myContext,
+                        "Unable to delete this drink...",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } catch(e: Exception) {
+                Toast.makeText(
+                    myContext,
+                    e.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 
     override fun getItemCount(): Int = values.size
@@ -50,4 +98,5 @@ class MyDrinksRecyclerViewAdapter(
         val deleteBtn: Button = binding.deleteDrink
     }
 
+    override val coroutineContext: CoroutineContext = Job()
 }
